041398eae7995aa0c54163e4114704124093d165
LeeJeongGi <apple4rhk@naver.com>
2022-03-08 21:33:58 +0900

    feat : HttpServletResponse - HTML 응답

--- src/main/java/hellomvc/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hellomvc/servlet/basic/response/ResponseHeaderServlet.java
@@ -46,7 +46,8 @@ public class ResponseHeaderServlet extends HttpServlet {
     }
 
     private void cookie(HttpServletResponse response) {
-        //Set-Cookie: myCookie=good; Max-Age=600; //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        //Set-Cookie: myCookie=good; Max-Age=600;
+        // response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
         Cookie cookie = new Cookie("myCookie", "good");
         cookie.setMaxAge(600); //600초
         response.addCookie(cookie);
